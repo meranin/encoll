0c228d08980c1da38e52ba619b422bc15dfe83a2
meranin <tsukisika@gmail.com>
2022-07-18 13:57:35 +0900

    ブログ投稿2

--- blog/blog/app/Http/Controllers/PostController.php
+++ blog/blog/app/Http/Controllers/PostController.php
@@ -9,8 +9,8 @@ class PostController extends Controller
 {
     public function index(Post $post)
     {
-        return view('posts/index')->with(['posts' => $post->get()]);  
-    }
+        return view('posts/index')->with(['posts' => $post->getPaginateByLimit()]);
+    } 
     
     public function show(Post $post)
     {
@@ -20,6 +20,13 @@ class PostController extends Controller
     public function create()
     {
         return view('posts/create');
+    }
+    
+    public function store(Request $request, Post $post)
+    {
+        $input = $request['post'];
+        $post->fill($input)->save();
+        return redirect('/posts/' . $post->id);
     }
 }
 ?>
